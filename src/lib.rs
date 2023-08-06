0ad739017d986eb7f7c31f2b4ac7d46e762c58ee
Samuel Vanderwaal <samuel.vanderwaal@gmail.com>
2023-08-05 21:29:09 -0500

    add docs

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,51 @@ use error::JibError;
 
 const MAX_TX_LEN: usize = 1232;
 
+/// The Jib struct is the main entry point for the library.
+/// It is used to create a new Jib instance, set the RPC URL, set the instructions, pack the instructions into transactions
+/// and finally hoist the transactions to the network.
+/// ```rust
+/// fn main() -> Result<()> {
+///     // Load our keypair file.
+///     let keypair = solana_sdk::signature::read_keypair_file("keypair.json").unwrap();
+///
+///     // Initialize Jib with our keypair.
+///     let mut jib = Jib::new(vec![keypair])?;
+///
+///     let mut instructions = vec![];
+///
+///     // Load mint addresses from a file.
+///     let addresses: Vec<String> =
+///         serde_json::from_reader(std::fs::File::open("collection_mints.json")?)?;
+///
+///     // Create an instruction for each mint.
+///     for address in addresses {
+///         let metadata = derive_metadata_pda(&Pubkey::from_str(&address).unwrap());
+///
+///         let ix = update_metadata_accounts_v2(
+///             mpl_token_metadata::ID,
+///             metadata,
+///             // Jib takes the payer by value but we can access it via this fn.
+///             jib.payer().pubkey(),
+///             None,
+///             None,
+///             Some(true),
+///             None,
+///         );
+///
+///         instructions.push(ix);
+///     }
+///
+///     // Set the instructions to be executed.
+///     jib.set_instructions(instructions);
+///
+///     // Run it. This executes on devnet by default.
+///     jib.hoist()?;
+///
+///     Ok(())
+/// }
+/// ```
+
 pub struct Jib {
     tpu_client: TpuClient,
     signers: Vec<Keypair>,
@@ -23,6 +68,7 @@ pub struct Jib {
 }
 
 impl Jib {
+    /// Create a new Jib instance. You should pass in all the signers you want to use for the transactions.
     pub fn new(signers: Vec<Keypair>) -> Result<Self, JibError> {
         let tpu_client = Self::create_tpu_client("https://api.devnet.solana.com")?;
 
@@ -47,22 +93,32 @@ impl Jib {
         Ok(tpu_client)
     }
 
+    /// Set the RPC URL to use for the transactions. This defaults to the public devnet URL.
     pub fn set_rpc_url(&mut self, url: &str) {
         self.tpu_client = Self::create_tpu_client(url).unwrap();
     }
 
+    /// Set the instructions to use for the transactions. This should be a vector of instructions that you wish to submit to the network.
     pub fn set_instructions(&mut self, ixes: Vec<Instruction>) {
         self.ixes = ixes;
     }
 
+    /// Set the signers to use for the transactions. This should be a vector of signers that you wish to use to sign the transactions.
+    pub fn set_signers(&mut self, signers: Vec<Keypair>) {
+        self.signers = signers;
+    }
+
+    /// Get the RPC client that is being used by the Jib instance.
     pub fn rpc_client(&self) -> &RpcClient {
         self.tpu_client.rpc_client()
     }
 
+    /// Get the first signer that is being used by the Jib instance, the transaction fee payer.
     pub fn payer(&self) -> &Keypair {
         self.signers.first().unwrap()
     }
 
+    /// Pack the instructions into transactions. This will return a vector of transactions that can be submitted to the network.
     pub fn pack(&mut self) -> Result<Vec<Transaction>, JibError> {
         debug!(
             "Commitment level: {:?}",
@@ -113,6 +169,7 @@ impl Jib {
         Ok(packed_transactions)
     }
 
+    /// Pack the instructions into transactions and submit them to the network via the TPU client. This will return a spinner while the transactions are being submitted.
     pub fn hoist(mut self) -> Result<(), JibError> {
         let packed_transactions = self.pack()?;
 
